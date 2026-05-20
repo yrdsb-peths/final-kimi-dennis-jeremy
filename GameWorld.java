beaf42289f8ea5e0fa1a4e73b7fc453de8cefe8b
Kimi Qin <440041588@gapps.yrdsb.ca>
2026-05-20 18:29:17 -0400

    add lightning

--- GameWorld.java
+++ GameWorld.java
@@ -1,23 +1,125 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class GameWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class GameWorld extends World
 {
+    public AureaSolvine aureaSolvine;
 
-    /**
-     * Constructor for objects of class GameWorld.
-     * 
-     */
+    int enemySpawnTimer = 0;
+    int lightningTimer = 0;
+
     public GameWorld()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1500, 750, 1); 
-        AureaSolvine aureaSolvine = new AureaSolvine();
-        addObject(aureaSolvine,750, 375);
+    {
+        super(1500, 750, 1);
+
+        aureaSolvine = new AureaSolvine();
+
+        addObject(aureaSolvine, 300, 200);
+    }
+
+    public void act()
+    {
+        spawnEnemy();
+        spawnLightning();
+    }
+    
+    public void spawnLightning()
+    {
+        lightningTimer++;
+    
+        // 每1.5秒
+        if(lightningTimer >= 90)
+        {
+            lightningTimer = 0;
+    
+            Enemy closest = getClosestEnemy();
+    
+            if(closest != null)
+            {
+                addObject(
+                    new Lightning(),
+                    closest.getX(),
+                    closest.getY()
+                );
+            }
+        }
+    }
+
+    public void spawnEnemy()
+    {
+        enemySpawnTimer++;
+
+        if(enemySpawnTimer >= 60)
+        {
+            enemySpawnTimer = 0;
+
+            int x;
+            int y;
+
+            do
+            {
+                x = Greenfoot.getRandomNumber(getWidth());
+                y = Greenfoot.getRandomNumber(getHeight());
+
+            }
+            while(distanceToPlayer(x, y) < 200);
+
+            addObject(new Enemy(), x, y);
+        }
+    }
+
+    public double distanceToPlayer(int x, int y)
+    {
+        int dx = x - aureaSolvine.getX();
+        int dy = y - aureaSolvine.getY();
+
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    
+    public Enemy getClosestEnemy()
+    {
+        java.util.List<Enemy> enemies =
+            getObjects(Enemy.class);
+    
+        if(enemies.isEmpty())
+        {
+            return null;
+        }
+    
+        Enemy closest = enemies.get(0);
+    
+        double closestDistance =
+            distanceBetween(
+                aureaSolvine.getX(),
+                aureaSolvine.getY(),
+                closest.getX(),
+                closest.getY()
+            );
+    
+        for(Enemy enemy : enemies)
+        {
+            double distance =
+                distanceBetween(
+                    aureaSolvine.getX(),
+                    aureaSolvine.getY(),
+                    enemy.getX(),
+                    enemy.getY()
+                );
+    
+            if(distance < closestDistance)
+            {
+                closestDistance = distance;
+                closest = enemy;
+            }
+        }
+    
+        return closest;
+    }
+    
+    public double distanceBetween(int x1, int y1, int x2, int y2 )
+    {
+        int dx = x1 - x2;
+        int dy = y1 - y2;
+    
+        return Math.sqrt(dx * dx + dy * dy);
     }
 }
---
+++ Lightning.java
@@ -0,0 +1,70 @@
+import greenfoot.*;
+import java.util.List;
+
+public class Lightning extends Actor
+{
+    static GreenfootImage[] frames = new GreenfootImage[16];
+
+    static
+    {
+        for(int i = 0; i < 16; i++)
+        {
+            frames[i] = new GreenfootImage(
+                "lightning/lightning" + i + ".png"
+            );
+
+            frames[i].scale(
+                frames[i].getWidth() / 3,
+                frames[i].getHeight() / 3
+            );
+        }
+    }
+
+    int frame = 0;
+    int timer = 0;
+
+    public Lightning()
+    {
+        setImage(frames[0]);
+    }
+
+    public void act()
+    {
+        animate();
+    
+        if(getWorld() == null)
+        {
+            return;
+        }
+    
+        hitEnemy();
+    }
+
+    public void animate()
+    {
+        timer++;
+
+        if(timer % 3 == 0)
+        {
+            frame++;
+
+            if(frame >= frames.length)
+            {
+                getWorld().removeObject(this);
+                return;
+            }
+
+            setImage(frames[frame]);
+        }
+    }
+
+    public void hitEnemy()
+    {
+        Enemy enemy = (Enemy)getOneIntersectingObject(Enemy.class);
+
+        if(enemy != null)
+        {
+            getWorld().removeObject(enemy);
+        }
+    }
+}
