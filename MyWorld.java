a1c7337dbc6b95a9a259236e4a6dfb2988167848
Denis126-dot <denishui126@gmail.com>
2026-05-20 12:05:52 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -4,7 +4,6 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
         
-        LeonClovis hero = new LeonClovis();
-        addObject(hero, 300, 200);
+        
     }
 }
