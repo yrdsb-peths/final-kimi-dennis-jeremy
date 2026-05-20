ae12511ad548463bad32379741d193cb93c7fa48
Denis126-dot <denishui126@gmail.com>
2026-05-20 11:26:31 -0400

    remove the ppl

--- MyWorld.java
+++ MyWorld.java
@@ -3,8 +3,5 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        
-        ppl p = new ppl();
-        addObject(p , 300, 200);
     }
 }
