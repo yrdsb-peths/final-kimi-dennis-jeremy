c31dde311db6993b27293700be4358bd6ea402da
jeremythcheung <jeremythcheung@gmail.com>
2026-05-20 11:52:06 -0400

    Characters

---
+++ AureaSolvine.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class AureaSolvine here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AureaSolvine extends Actor
+{
+    /**
+     * Act - do whatever the AureaSolvine wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
