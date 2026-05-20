fceec8d68e0b7c908f5230ab42852843ff75f70d
Kimi Qin <440041588@gapps.yrdsb.ca>
2026-05-20 11:51:58 -0400

    background

--- GameWorld.java
+++ GameWorld.java
@@ -16,6 +16,6 @@ public class GameWorld extends World
     public GameWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1200, 800, 1); 
+        super(1500, 750, 1); 
     }
 }
