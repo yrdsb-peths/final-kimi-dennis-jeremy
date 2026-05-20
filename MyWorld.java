6d3576b40ad6a43672faeb2a9d887d92c2319949
Denis126-dot <denishui126@gmail.com>
2026-05-20 11:24:37 -0400

    testing

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        ppl p = new ppl();
+        addObject(p , 300, 200);
     }
 }
