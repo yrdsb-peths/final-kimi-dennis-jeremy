033d7f6e92cc21bacdf6739535b649cc4a216315
Denis126-dot <denishui126@gmail.com>
2026-05-20 11:58:40 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        LeonClovis hero = new LeonClovis();
+        addObject(hero, 300, 200);
     }
 }
