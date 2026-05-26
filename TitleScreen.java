7edebdf123c394acb547e50ff46bc91cf5035d3e
Denis126-dot <denishui126@gmail.com>
2026-05-26 11:05:14 -0400

    Create TitleScreen.java

---
+++ TitleScreen.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends MyWorld
+{
+
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {
+    }
+}
